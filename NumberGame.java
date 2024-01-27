85e3879c0d7e4c4be5308072730ec82aff183883
JayeshShankarJadhav <157900773+JayeshShankarJadhav@users.noreply.github.com>
2024-01-27 20:38:49 +0530

    Create NumberGame.java

---
+++ NumberGame.java
@@ -0,0 +1,56 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int lowerBound = 1;
+        int upperBound = 100;
+        int maxAttempts = 10;
+        int score = 0;
+
+        System.out.println("Welcome to the Number Game!");
+        System.out.println("In This Game You Have To Guess A Number Between 1 to 100,\nYou Will Get Total 10 Attempts To Guess The Number.");
+        System.out.println("Please Enter You Full Name:\n");
+        // Read the user's input as a string
+        String name = scanner.nextLine();
+
+        System.out.println("All The Best\n"+name);
+        do {
+            int targetNumber = random.nextInt(upperBound - lowerBound + 1) + lowerBound;
+            int attempts = 0;
+            boolean guessedCorrectly = false;
+
+            System.out.println("\nGuess the number between " + lowerBound + " and " + upperBound);
+
+            while (attempts < maxAttempts) {
+                System.out.print("Enter your guess: ");
+                int userGuess = scanner.nextInt();
+                attempts++;
+
+                if (userGuess == targetNumber) {
+                    guessedCorrectly = true;
+                    break;
+                } else if (userGuess < targetNumber) {
+                    System.out.println("Too low! Try again.");
+                } else {
+                    System.out.println("Too high! Try again.");
+                }
+            }
+
+            if (guessedCorrectly) {
+                score++;
+                System.out.println("Congratulations! You guessed the number in " + attempts + " attempts.");
+            } else {
+                System.out.println("Sorry! You've reached the maximum number of attempts. The correct number was: " + targetNumber);
+            }
+
+            System.out.print("Do you want to play again? (yes/no): ");
+        } while (scanner.next().equalsIgnoreCase("yes"));
+
+        System.out.println("Game over! Your final score is: " + score);
+        scanner.close();
+    }
+}
